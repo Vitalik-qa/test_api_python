94bc4ed024d691e2b64aa7c7707d84a136f6d01f
Wita <you@example.com>
2025-05-15 17:58:14 +0300

    Добавление переменных 2часть

--- resources/urls.py
+++ resources/urls.py
@@ -1,5 +1,8 @@
 main_url = "https://petstore.swagger.io/v2/"
 url_pet_post = main_url + "pet"
+url_findByStatus = "https://petstore.swagger.io/v2/pet/findByStatus"
 def url_pet_get_id(pet_id):
     return main_url + "pet/" + pet_id
+def url_uploadImage(pet_id):
+    return main_url + "pet/" + pet_id + "/uploadImage"
 
